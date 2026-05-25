c1eaac074f1e0353403ce18de095dc8eb63ddb10
King-luiz <kingluiz47@gmail.com>
2026-05-25 12:32:06 +0300

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
-//const express = require('express');
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 import dotenv from "dotenv";
-import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
-import productRoutes from './routes/product.route.js';
-
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 app.use("/api/products", productRoutes);
 
+
+console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")) );  
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-    });
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "frontend", "dist", "index.html")
+    );
+  });
 }
-const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-// postman desktop application
-app.listen(PORT, () =>{
-    connectDB();
-    console.log('server started at http://localhost:' + PORT);
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server started at http://localhost:${PORT}`);
 });
-
